253b6109500c848f9ee4fe2e028db1a4f8cf283c
Linh Nguyen <linhnguyen1998125@gmail.com>
2019-11-26 22:34:32 +0700

    edit style table

--- app/Http/Controllers/HocsinhController.php
+++ app/Http/Controllers/HocsinhController.php
@@ -21,21 +21,40 @@ class HocsinhController extends Controller
         $hocsinh = DB::select('select * from hocsinh where MaHS = ?',[$MaHS]);
         return view('UpdateHocSinh',['hocsinh'=>$hocsinh]);
      }
+
      public function ediths(Request $request, $MaHS) {
-         
-        $hocsinh = DB::select('select * from hocsinh');
+     
+  
         $name = $request->input('stud_name');
         $namsinh = $request->input('namsinh');
         $gioitinh = $request->input('gioitinh');
         $diachi = $request->input('diachi');
         $malop = $request->input('malop');
-        DB::update('update hocsinh set HoTen = ? , NamSinh = ? , GioiTinh = ?, 	DiaChi = ? , MaLop = ?   where MaHS = ?',[$name,$namsinh,$gioitinh,$diachi,$malop,$MaHS]);
-        echo '<script>alert("Update thành công nha!!!");</script>';
-        echo '<h1 align="center"> Update thành công</h1>';
-        echo '<a href = "/HocSinh">Trở lại trang Học Sinh</a> ';
-       
-       
+        $reuld = DB::update('update hocsinh set HoTen = ? , NamSinh = ? , GioiTinh = ?, DiaChi = ? , MaLop = ?   where MaHS = ?',[$name,$namsinh,$gioitinh,$diachi,$malop,$MaHS]);
+        $reuld;
+        if($reuld==true){
+            //echo '<script>alert("Update thành công nha !!!");</script> ';
+            return redirect('HocSinh')->with('status', 'Update thành công nha !!!');
+        }
+        else{
+         
+            return redirect('HocSinh')->with('status', 'Bị lỗi rồi  !!!');
+        }
+        // return redirect()->action('HocsinhController@HocSinh');
+        // return redirect()->away('http://127.0.0.1:8000/HocSinh');
+        // return redirect()->action('HocsinhController@HocSinh');
+        // echo '<h1 align="center"> Update thành công</h1>';
+        // echo '<a href = "/HocSinh">Trở lại trang Học Sinh</a> ';
+        
      }
-
+     public function delete($MaHS)
+        {   
+        
+            $dele = DB::find($MaHS);
+            $dele->delete($MaHS);
+            return response()->json([
+                'success' => 'Record has been deleted successfully!'
+            ]);
+        }
  
 }
