a07e31d4118d20d58134f0b8731fe84c06053737
tiencuongtran <tiencuongtran.0211@gmail.com>
2020-01-03 09:40:18 +0700

    Update MonHocController.php

--- app/Http/Controllers/MonHocController.php
+++ app/Http/Controllers/MonHocController.php
@@ -36,8 +36,7 @@ class MonHocController extends Controller
     public function postThemMonHoc(Request $tmh1)
     {
         $tmh = new MonHoc();
-
-        $tmh->TenMH = $tmh1->TenMH;
+         $tmh->TenMH = $tmh1->TenMH;
         $tmh->save();
         return redirect()->route('MonHoc')->with(['xoa' => 'Đã Thêm Môn Học Thành Công']);
     }
@@ -60,4 +59,16 @@ class MonHocController extends Controller
         $smh = MonHoc::where('MaMH', $id)->first();
         return view('SuaMonHoc', compact('smh'));
     }
+
+    public function findMonHoc($fmh){
+        $fmh = MonHoc::where('TenMH', 'like', '%'.$fmh.'%')->get();
+
+        return view('KetQuaTimMonHoc', compact('fmh'));
+    }
+
+    public function XuLyfindMonHoc(Request $req){
+        return redirect()->route('KetQuaTimMonHoc', $req->search);
+    }
+
+
 }
